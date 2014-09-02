6c2648eadab57be4c7bc7c0c43a2b63b5c642267
Prelang Builder <builder@prelang.com>
2014-09-02 14:18:44 +0000

    Generated model PostComment with migration

---
+++ db/migrate/20140902141844_create_post_comments.rb
@@ -0,0 +1,10 @@
+class CreatePostComments < ActiveRecord::Migration
+  def change
+    create_table :post_comments do |t|
+      t.references :post, index: true
+      t.references :user, index: true
+
+      t.timestamps
+    end
+  end
+end
